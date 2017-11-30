0ee70ebf759e803e62b4fd1e7fac3b4d758b6b3b
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-30 00:46:20 -0800

    bad filepath error

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -5,23 +5,34 @@ const reader = require('../lib/reader');
 // const paths = [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`];
 
 describe('reader.test.js', () => {
-  test('should be 64 char', () => {
+  test('should be 64 char', done => {
     reader.readFile(
       [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
       (error, data) => {
         expect(error).toBeNull();
-        expect(error).toBe(64);
         expect(data.length).toEqual(64);
         done();
       });
   });
-  // test('should fail if not 64 char', () => {
-  //   reader.readFile(paths,(error, data) => {
-  //     expect(error).toBeNull();
-  //     expect(data).toEqual('something with 64 characters');
-  //     done();
-  //   });
-  // });
+
+  test('should throw error if bad file', done => {
+    reader.readFile(
+      [`${__dirname}/../less-than-6`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
+      (error, data) => {
+        expect(error).toBeTruthy();
+        expect(data).toBeNull();
+        done();
+      });
+  });
+
+  test('should throw error if not paths', () => {
+    reader.readFile(
+      'not paths',
+      (error, data) => {
+        expect(error).toBeTruthy();
+        expect(data).toBeNull();
+      });
+  });
 });
 // describe('file-reader.test.js', () => {
 //   test('should be 64 char', () => {
