05044bdaf09a62b1d2327dc3c970a8302e26dd04
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-29 22:55:49 -0800

    first test passes, working on figuring out why

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -3,6 +3,7 @@
 const reader = require('../lib/reader.js');
 
 const paths = [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`];
+
 const callback = (error, data) => {
   expect(error).toBeNull();
   expect(data).toEqual('something with 64 characters');
@@ -10,8 +11,17 @@ const callback = (error, data) => {
 };
 
 describe('reader.test.js', () => {
-  test('should be 64 char'), () => {
+  test('should be 64 char', () => {
     reader.readFile(paths, callback);
-  };
+  });
 }
 );
+// describe('file-reader.test.js', () => {
+//   test('should be 64 char', () => {
+//     fileReader.readFile((error, data) => {
+//       expect(error).toBeNull();
+//       expect(data).toEqual('something with 64 characters');
+//       done();
+//     });
+//   });
+// });
