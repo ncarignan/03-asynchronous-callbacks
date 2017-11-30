6bed2d6008341122fb1b9ca224077887de61a924
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-30 09:41:42 -0800

    in class recursive file-reader

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lab 03
+
+## exports
+
+###reader
+reader exports four customized functions that affect arrays
+####reader.readFile
+takes in an array of paths and a callback function, and returns an array stringified files. it also catches errors and returns a descriptive error message
--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -2,36 +2,48 @@
 
 const reader = require('../lib/reader');
 
-// const paths = [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`];
-
 describe('reader.test.js', () => {
-  test('should be 64 char', done => {
-    reader.readFile(
-      [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
-      (error, data) => {
-        expect(error).toBeNull();
-        expect(data.length).toEqual(64);
-        done();
-      });
-  });
+  // test('should be 64 char', done => {
+  //   reader.readFile(
+  //     [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
+  //     (error, data) => {
+  //       expect(error).toBeNull();
+  //       expect(data.length).toEqual(64);
+  //       done();
+  //     });
+  // });
+  //
+  // test('should throw error if bad file', done => {
+  //   reader.readFile(
+  //     [`${__dirname}/../less-than-6`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
+  //     (error, data) => {
+  //       expect(error).toBeTruthy();
+  //       expect(data).toBeNull();
+  //       done();
+  //     });
+  // });
+  //
+  // test('should throw error if not paths', () => {
+  //   reader.readFile(
+  //     'not paths',
+  //     (error, data) => {
+  //       expect(error).toBeTruthy();
+  //       expect(data).toBeNull();
+  //     });
+  // });
 
-  test('should throw error if bad file', done => {
-    reader.readFile(
-      [`${__dirname}/../less-than-6`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
-      (error, data) => {
-        expect(error).toBeTruthy();
-        expect(data).toBeNull();
-        done();
-      });
-  });
-
-  test('should throw error if not paths', () => {
-    reader.readFile(
-      'not paths',
-      (error, data) => {
-        expect(error).toBeTruthy();
-        expect(data).toBeNull();
-      });
+  test('file contents should be returned in order based on input array', done => {
+    let filePaths = [
+      `${__dirname}/../assets/mario.txt`,
+      `${__dirname}/../assets/peach.txt`,
+      `${__dirname}/../assets/luigi.txt`,
+    ];
+    reader.readFiles(filePaths, (error,data) => {
+      expect(data[0]).toEqual('mario');
+      expect(data[1]).toEqual('peach');
+      expect(data[2]).toEqual('luigi');
+      done();
+    });
   });
 });
 // describe('file-reader.test.js', () => {
