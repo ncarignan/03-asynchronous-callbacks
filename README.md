c4540685d20225570668cfa8136e07cbb4d8998d
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-30 09:45:37 -0800

    updated readme, one of the tests still fails

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 reader exports four customized functions that affect arrays
 ####reader.readFile
 takes in an array of paths and a callback function, and returns an array stringified files. it also catches errors and returns a descriptive error message
+####reader.readFiles
+takes in an array of paths and a callback function, and returns an array stringified files. it also catches errors and returns a descriptive error message. The files will stay arranged as they come in because of the recursive nature of the function.
+#### NOTES
+I am now in the process of refactoring to recursive functions and fixing my original functionality. The readFiles function is structured like the one during code review, I intend to improve on my testing.
--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -3,34 +3,34 @@
 const reader = require('../lib/reader');
 
 describe('reader.test.js', () => {
-  // test('should be 64 char', done => {
-  //   reader.readFile(
-  //     [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
-  //     (error, data) => {
-  //       expect(error).toBeNull();
-  //       expect(data.length).toEqual(64);
-  //       done();
-  //     });
-  // });
-  //
-  // test('should throw error if bad file', done => {
-  //   reader.readFile(
-  //     [`${__dirname}/../less-than-6`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
-  //     (error, data) => {
-  //       expect(error).toBeTruthy();
-  //       expect(data).toBeNull();
-  //       done();
-  //     });
-  // });
-  //
-  // test('should throw error if not paths', () => {
-  //   reader.readFile(
-  //     'not paths',
-  //     (error, data) => {
-  //       expect(error).toBeTruthy();
-  //       expect(data).toBeNull();
-  //     });
-  // });
+  test('should be 64 char', done => {
+    reader.readFile(
+      [`${__dirname}/../DIRECTIONS.md`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
+      (error, data) => {
+        expect(error).toBeNull();
+        expect(data.length).toEqual(64);
+        done();
+      });
+  });
+
+  test('should throw error if bad file', done => {
+    reader.readFile(
+      [`${__dirname}/../less-than-6`, `${__dirname}/../lib/reader.js`, `${__dirname}/../__test__/reader.test.js`],
+      (error, data) => {
+        expect(error).toBeTruthy();
+        expect(data).toBeNull();
+        done();
+      });
+  });
+
+  test('should throw error if not paths', () => {
+    reader.readFile(
+      'not paths',
+      (error, data) => {
+        expect(error).toBeTruthy();
+        expect(data).toBeNull();
+      });
+  });
 
   test('file contents should be returned in order based on input array', done => {
     let filePaths = [
